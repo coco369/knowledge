5a995388a78dcef092462b7871d7d8c1bc49e783
coco369 <1571236356@qq.com>
2019-11-28 17:50:04 +0800

    go指针

--- go/1.Go基础入门/基础代码/6.1Go指针使用.go
+++ go/1.Go基础入门/基础代码/6.1Go指针使用.go
@@ -11,8 +11,18 @@ func change(a, b *int) {
 	*a, *b = *b, *a
 }
 
+func change2(a, b int) (int, int) {
+	return b, a
+}
+
 func main () {
+	// 交换两个值，使用指针，方法1
 	a, b := 3, 4
 	change(&a, &b)
 	fmt.Println(a, b)
+
+	// 交换两个值，不实用指针，方法2
+	c, d := 3, 4
+	c, d = change2(c, d)
+	fmt.Println(c, d)
 }
