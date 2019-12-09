0409c079600e306a688acc266619790bfad7606d
coco369 <1571236356@qq.com>
2019-12-09 20:05:36 +0800

    iris的温昂

--- go/2.Go框架入门/iris框架/main.go
+++ go/2.Go框架入门/iris框架/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/kataras/iris"
-
+import (
+	"github.com/kataras/iris"
+)
 
 func main() {
+	// 生成iris对象
 	app := iris.New()
-	app.Get("/", func(ctx iris.Context){})
-	app.Run(iris.Addr(":8081"))
+
+	// 定义路由和方法的关联关系
+	app.Get("/hello", func(ctx iris.Context) {
+		// 写入返回给前端的数据："hello world"
+		ctx.WriteString("hello world")
+	})
+
+	// 启动，并指定启动的端口为8090
+	app.Run(iris.Addr(":8090"))
+
 }
